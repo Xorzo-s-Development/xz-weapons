2c814fc75624124c473182bf8423b4cce5a64f19
xorzo <dyltelwesemann@gmail.com>
2022-11-06 12:43:13 +0100

    commit
    commit

---
+++ client/main.lua
@@ -0,0 +1,29 @@
+CreateThread(function()
+	while true do
+        local ped = PlayerPedId()
+            if IsPedShooting(ped) and not IsPedDoingDriveby(ped) then
+                local _,wep = GetCurrentPedWeapon(PlayerPedId())
+                if Config.recoils[wep] and Config.recoils[wep] ~= 0 then
+                    ShakeGameplayCam('SMALL_EXPLOSION_SHAKE', Config.recoils[wep])
+                end
+            end
+
+            if Config.FirstPersonOnly then
+                if IsControlPressed(0, 25) then
+                    if GetFollowPedCamViewMode() == 4 and not check then
+                        check = false
+                    else
+                        SetFollowPedCamViewMode(4)
+                        check = true
+                    end
+                else
+                    DisablePlayerFiring(PlayerPedId(), true)
+                    if check then
+                        SetFollowPedCamViewMode(1)
+                        check = false
+                    end
+                end
+            end
+        Wait(0)
+	end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version "cerulean"
+lua54 'yes'
+
+games {
+  "gta5"
+}
+
+shared_script '@es_extended/imports.lua'
+
+client_script 'client/**/*.lua'
